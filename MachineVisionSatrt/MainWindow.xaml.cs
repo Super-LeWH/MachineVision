85c3cfe197b76bf88f6394a5a8a80efdf05871b3
lwh <weihua.le@origi-tech.com>
2025-07-21 17:42:19 +0800

    fix:set the version for software.

--- MachineVisionSatrt/MainWindow.xaml.cs
+++ MachineVisionSatrt/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,12 @@ namespace MachineVisionSatrt
         public MainWindow()
         {
             InitializeComponent();
+            // 设置应用的版本信息
+            var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+            // 按点分割，保留前3段
+            var parts = version.Split('.');
+            var shortFileVersion = string.Join(".", parts.Take(3));
+            this.Title = $"MachineVision V{shortFileVersion}";
         }
     }
 }
