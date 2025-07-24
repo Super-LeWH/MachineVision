dff84c59d3db05871d184ef3884803df69b23fde
lwh <weihua.le@origi-tech.com>
2025-07-24 13:19:44 +0800

    feat:add utils

---
+++ Utils/ViewModel/FilesDealViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.View;
+
+namespace Utils.ViewModel
+{
+    internal partial class FilesDealViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private object? currentView;
+        public FilesDealViewModel()
+        {
+            CurrentView = new PageOfReadWrite();
+        }
+
+        [RelayCommand]
+        private void ChangePage(string pageName)
+        {
+            switch (pageName)
+            {
+                case "PageOfReadWrite":
+                    CurrentView = new PageOfReadWrite();
+                    break;
+                case "PageB":
+                    CurrentView = new PageB();
+                    break;
+            }
+        }
+    }
+}
